6993dfe6180388db067b664ee4c9d6bd58a91029
Lassi Eloranta <8556790+Lassii-@users.noreply.github.com>
2021-03-30 02:57:13 +0300

    Make unicafe code a little prettier

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
 
-const Statistic = ({ text, counter }) => <tr><td>{text}</td><td>{counter}</td></tr>
-const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
-
 const Statistics = ({ good, neutral, bad }) => {
+  const Statistic = ({ text, counter }) => <tr><td>{text}</td><td>{counter}</td></tr>
   if (good === 0 && neutral === 0 && bad === 0) {
     return (
       <div>
@@ -34,14 +32,15 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+
+  const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
   return (
     <div>
       <h1>Give feedback</h1>
       <Button handleClick={() => setGood(good + 1)} text="Good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="Bad" />
-
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics {...{ good, neutral, bad }} />
     </div>
   )
 }
